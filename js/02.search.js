e886b11639942b5ff7cf0e0e63b265057d0b6181
SangYong-Jeong <mmaf@naver.com>
2021-08-13 11:21:22 +0900

    2교시 수업 완료

--- js/02.search.js
+++ js/02.search.js
@@ -1,9 +1,11 @@
 /************* Global init ***************/
 var auth = 'KakaoAK 4545d096ee04bdcea13013e722fa668f';
+var kakaoURL = 'https://dapi.kakao.com/';
+
 
 /************* user function *************/
 function getPath (cate) {
-	return 'https://dapi.kakao.com/'+(cate === 'book' ? 'v3' : 'v2')+'/search/'+cate;
+	return kakaoURL+(cate === 'book' ? 'v3' : 'v2')+'/search/'+cate;
 }
 
 function getParams(query) {
@@ -11,6 +13,44 @@ function getParams(query) {
 		params: {query : query},
 		headers: {Authorization: auth}
 	}
+}
+
+function setTotalCnt (cnt) {
+	$('.result-cnt').html(numberFormat(cnt));
+}
+
+function setWebLists (r) {
+	console.log(r);
+	$('.lists').empty();
+	r.forEach(function (v, i) {
+		var html  = '<li class="list web">';
+		html += '<a class="title" href="'+v.url+'" target="_blank">'+v.title+'</a>';
+		html += '<p class="content">'+v.contents+'</p>';
+		html += '<a class="link" href="'+v.url+'" target="_blank">'+v.url+'</a>';
+		html += '<div class="dt">'+moment(v.datetime).format('YYYY-MM-DD HH:mm:ss')+'</div>';
+		html += '</li>';
+		$('.lists').append(html);
+	});
+}
+
+function setImageLists (r) {
+
+}
+
+function setClipLists (r) {
+
+}
+
+function setBlogLists (r) {
+
+}
+
+function setBookLists (r) {
+
+}
+
+function setCafeLists (r) {
+
 }
 
 /************* event callback ************/
@@ -22,7 +62,29 @@ function onSubmit(e) {
 }
 
 function onSuccess (res) {
-	console.log(res);
+	var cate = res.config.url.split('/').pop();
+	var v = res.data;
+	setTotalCnt(v.meta.total_count);
+	switch(cate) {
+		case 'web':
+			setWebLists(v.documents);
+			break;
+		case 'image':
+			setImageLists(v.documents);
+			break;
+		case 'vclip':
+			setClipLists(v.documents);
+			break;
+		case 'blog':
+			setBlogLists(v.documents);
+			break;
+		case 'book':
+			setBookLists(v.documents);
+			break;
+		case 'cafe':
+			setCafeLists(v.documents);
+			break;
+	}
 }
 
 function onError(err) {
